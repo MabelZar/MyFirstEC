807579035d1f548ebe7d18bc8eaf751c75ba4b00
MabelZar <mabel.zar.esp@gmail.com>
2023-11-14 00:54:41 +0100

    nav done

---
+++ data.js
@@ -0,0 +1,58 @@
+const jsonObj = {
+  "tipo de prenda": "Abrigos",
+  lugar: "España",
+  tendencia: 2023,
+  descripcion: "Abrigos confeccionado con lana etc etc...",
+  articulos: [
+    {
+      nombre: "abrigo de lana",
+      precio: 200 + " $",
+      imagen: "../images/lana.jpg",
+      composicion: "composicion : 53% lana",
+      categoria: "ABRIGOS",
+
+      tallas: ["xs", "s", "m", "l", "xl"],
+    },
+    {
+      nombre: "acolchado",
+      precio: 250,
+      imagen: "../images/plumifero.jpg",
+      composicion: "composicion : 53% lana",
+      categoria: "ABRIGOS",
+      tallas: ["xs", "s", "m"],
+    },
+    {
+      nombre: "plumifero",
+      precio: 350,
+      imagen: "../images/acolchado.jpg",
+      composicion: "composicion : plumas",
+      categoria: "ABRIGOS",
+      tallas: ["xs", "s", "m", "l"],
+    },
+    {
+      nombre: "bolso feo",
+      precio: 777,
+      imagen: "../images/bolso-solapa-bolitas.jpg",
+      composicion: "composicion : cuero",
+      categoria: "BOLSOS",
+      tallas: ["tu"],
+    },
+  ],
+};
+
+const jsonCat = {
+  categoria: [
+    {
+      idCategoria: 0,
+      nombreCategoria: "abrigos"
+    },
+    {
+      idCategoria: 2,
+      nombreCategoria: "bolsos"
+    },
+    {
+      idCategoria: 3,
+      nombreCategoria: "pantalones"
+    },
+  ],
+};
---
+++ functions.js
@@ -0,0 +1,72 @@
+const section = document.getElementById("section");
+const header = document.getElementById("header");
+//-------------------------------
+function populateHeader() {
+  const myH1 = document.createElement("h1");
+  myH1.textContent = jsonObj["tipo de prenda"];
+  header.appendChild(myH1);
+
+  const myP = document.createElement("h2");
+  myP.textContent = "Tendencia: " + jsonObj.tendencia;
+  header.appendChild(myP);
+}
+
+function populateNav() {
+  let cat = jsonCat["categoria"];
+  let myNab = document.getElementsByTagName("nav");//get tag devuelve un array
+  for (var i = 0; i < cat.length; i++) {
+    let enlace = document.createElement('a');
+    enlace.href='/categorias/category-page.html?parametroCategoria=' + cat[i].idCategoria;
+    enlace.textContent = cat[i].nombreCategoria;
+    let parrafo = document.createElement('p');
+    parrafo.appendChild(enlace);
+    myNab[0].appendChild(parrafo);//obtengo la posicion 0
+  }
+}
+
+function show() {
+  let parametro = new URL(location.href).searchParams.get("categoryParametro");
+
+  const articulos = jsonObj["articulos"];
+
+  for (var i = 0; i < articulos.length; i++) {
+    if (articulos[i].categoria === parametro) {
+      const myArticle = document.createElement("article");
+      myArticle.textContent = articulos[i].nombre;
+
+      const img = document.createElement("img");
+      img.src = articulos[i].imagen;
+
+      const myP1 = document.createElement("p");
+      myP1.textContent = articulos[i].precio;
+
+      const myP2 = document.createElement("p");
+      myP2.textContent = articulos[i].composicion;
+
+      const myP3 = document.createElement("p");
+      myP3.textContent = "tallas:";
+
+      const myList = document.createElement("ul");
+
+      const tallas = articulos[i].tallas;
+
+      for (var j = 0; j < tallas.length; j++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = tallas[j];
+        myList.appendChild(listItem);
+      }
+
+      myArticle.appendChild(img);
+      myArticle.appendChild(myP1);
+      myArticle.appendChild(myP2);
+      myArticle.appendChild(myP3);
+      myArticle.appendChild(myList);
+      section.appendChild(myArticle);
+    }
+  }
+}
+//console.info(jsonObj)
+//console.info(jsonObj.articulos[0].tallas[0])
+populateHeader();
+populateNav();
+show();
