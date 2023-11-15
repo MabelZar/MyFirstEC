9e0f4eef27ca669f8e2dcf6383b541faddcd1778
MabelZar <mabel.zar.esp@gmail.com>
2023-11-15 23:06:31 +0100

    save product

--- data.js
+++ data.js
@@ -2,40 +2,70 @@ const jsonObj = {
   marca: "SIN MARCA",
   lugar: "España",
   tendencia: 2023,
-  descripcion: "Abrigos confeccionado con lana etc etc...",
+  descripcion: "ropa ",
   articulos: [
     {
+     idProducto:'001',
       nombre: "abrigo de lana",
       precio: 200 + " $",
       imagen: "../images/lana.jpg",
       composicion: "composicion : 53% lana",
-      categoria: "ABRIGOS",
-
+      categoria: "0",
       tallas: ["xs", "s", "m", "l", "xl"],
     },
     {
+      idProducto:'002',
       nombre: "acolchado",
-      precio: 250,
+      precio: 250 + " $",
       imagen: "../images/plumifero.jpg",
       composicion: "composicion : 53% lana",
-      categoria: "ABRIGOS",
+      categoria: "0",
       tallas: ["xs", "s", "m"],
     },
     {
+      idProducto:'003',
       nombre: "plumifero",
-      precio: 350,
+      precio: 350 + " $",
       imagen: "../images/acolchado.jpg",
       composicion: "composicion : plumas",
-      categoria: "ABRIGOS",
+      categoria: "0",
       tallas: ["xs", "s", "m", "l"],
     },
     {
-      nombre: "bolso feo",
-      precio: 777,
+      idProducto:'004',
+      nombre: "bolso de cuero",
+      precio: 200 + " $",
       imagen: "../images/bolso-solapa-bolitas.jpg",
       composicion: "composicion : cuero",
-      categoria: "BOLSOS",
-      tallas: ["tu"],
+      categoria: "1",
+      tallas: [""],
+    },
+    {
+      idProducto:'005',
+      nombre: "bolso de piel",
+      precio: 450 + " $",
+      imagen: "../images/bolso de piel.jpeg",
+      composicion: "composicion : piel",
+      categoria: "1",
+      tallas: [""],
+    },
+    {
+      idProducto:'006',
+      nombre: "jean",
+      precio: 55+ " $",
+      imagen: "../images/jean.jpeg",
+      composicion: "composicion : plumas",
+      categoria: "2",
+      tallas: ["xs", "s", "m", "l"],
+    },
+    {
+      idProducto:'007',
+      nombre: "satre",
+      precio: 90 + " $",
+      imagen: "../images/sastre.jpeg",
+      composicion: "composicion : plumas",
+      categoria: "2",
+      tallas: ["xs", "s", "m", "l"],
     },
   ],
 };
@@ -44,7 +74,7 @@ const jsonCat = {
   categoria: [
     {
       idCategoria: 0,
-      nombreCategoria: "Abrigoss"
+      nombreCategoria: "Abrigos"
     },
     {
       idCategoria: 1,
@@ -56,3 +86,5 @@ const jsonCat = {
     },
   ],
 };
+//-------------------
+
--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 const section = document.getElementById("section");
-//const header = document.getElementById("header");
+const header = document.getElementById("header");
 //-------------------------------
 function populateHeader() {
   const myH1 = document.createElement("h1");
@@ -13,31 +13,32 @@ function populateHeader() {
 
 function populateNav() {
   let cat = jsonCat["categoria"];
-  let myNab = document.getElementsByTagName("nav");//get tag devuelve un array
+  let myNav = document.getElementsByTagName("nav");//get tag devuelve un array
   let linea =document.createElement('hr');
   for (var i = 0; i < cat.length; i++) {
     let enlace = document.createElement('a');
     enlace.href='/categorias/category-page.html?parametroCategoria=' + cat[i].idCategoria;
     enlace.textContent = ' '+cat[i].nombreCategoria;
-    // let parrafo = document.createElement('p');
-    //parrafo.appendChild(enlace);
-    myNab[0].appendChild(enlace);//obtengo la posicion 0
-    // enlace.style.display:
-    myNab[0].appendChild(linea);
+    myNav[0].appendChild(enlace);//obtengo la posicion 0
+    myNav[0].appendChild(linea);
   }
 }
 
-
-
-function show() {
-  let parametro = new URL(location.href).searchParams.get("categoryParametro");
+function mostrarArticuloPorCategoria() {
+  let parametro = new URL(location.href).searchParams.get("parametroCategoria");
   const articulos = jsonObj["articulos"];
-  //probando funcion mostrar un solo producto
+  
 
- 
-  //if(parametro===)
   for (var i = 0; i < articulos.length; i++) {
     if (articulos[i].categoria === parametro) {
+
+      //let parametroProducto = new URL(location.href).searchParams.get("parametroProducto");
+
+      let enlaceProducto=document.createElement('a');
+      enlaceProducto.href='/productos/productos.html?parametroProducto='+ articulos[i].idProducto;
+      enlaceProducto.textContent= articulos[i].idProducto;
+     
+      
       const myArticle = document.createElement("article");
       myArticle.textContent = articulos[i].nombre;
 
@@ -49,6 +50,10 @@ function show() {
 
       const myP2 = document.createElement("p");
       myP2.textContent = articulos[i].composicion;
+
+      let btnComprar=document.createElement('button');
+      btnComprar.textContent='Agregar';
+      btnComprar.href='/carrito/carrito.html?parametroCarrito='
 
       const myP3 = document.createElement("p");
       myP3.textContent = "tallas:";
@@ -62,18 +67,66 @@ function show() {
         listItem.textContent = tallas[j];
         myList.appendChild(listItem);
       }
-
+      myArticle.appendChild(enlaceProducto);
       myArticle.appendChild(img);
       myArticle.appendChild(myP1);
       myArticle.appendChild(myP2);
       myArticle.appendChild(myP3);
       myArticle.appendChild(myList);
       section.appendChild(myArticle);
+      myArticle.appendChild(btnComprar);
     }
   }
 }
-//console.info(jsonObj)
-//console.info(jsonObj.articulos[0].tallas[0])
-populateHeader();
-populateNav();
-show();
+//function mostrarArticuloPorID(){
+
+  let parametro = new URL(location.href).searchParams.get("parametroProducto");
+  const articulos = jsonObj["articulos"];
+
+  for (var i = 0; i < articulos.length; i++) {
+    if (articulos[i].idProducto === parametro) {
+
+      const myArticle = document.createElement("article");
+      myArticle.textContent = articulos[i].nombre;
+
+      const img = document.createElement("img");
+      img.src = articulos[i].imagen;
+
+      const myP1 = document.createElement("p");
+      myP1.textContent = articulos[i].precio;
+
+      const myP2 = document.createElement("p");
+      myP2.textContent = articulos[i].composicion;
+      //agregando boton
+      let btnComprar=document.createElement('button');
+      btnComprar.textContent='Agregar';
+      btnComprar.style.color='yelow';
+
+      const myP3 = document.createElement("p");
+      myP3.textContent = "tallas:";
+
+      const myList = document.createElement("ul");
+
+      const tallas = articulos[i].tallas;
+
+      for (var j = 0; j < tallas.length; j++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = tallas[j];
+        myList.appendChild(listItem);
+      }
+      myArticle.appendChild(img);
+      myArticle.appendChild(myP1);
+      myArticle.appendChild(myP2);
+      
+      myArticle.appendChild(myP3);
+      myArticle.appendChild(myList);
+      section.appendChild(myArticle);
+      myArticle.appendChild(btnComprar);
+    }
+    //break;
+  }
+  
+
+
+//}
+
